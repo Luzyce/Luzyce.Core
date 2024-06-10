cf07af3d1acab2faa7c9c9f0933277caa2cf59ee
Daniel <daniel.dominik.nowicki@gmail.com>
2024-06-10 14:04:09 +0200

    Update property name in GetDocumentResponseDto to match convention

--- Luzyce.Core/Models/Document/GetDocumentResponseDto.cs
+++ Luzyce.Core/Models/Document/GetDocumentResponseDto.cs
@@ -9,7 +9,7 @@ public class GetDocumentResponseDto
     public GetWarehouseResponseDto Warehouse { get; set; } = new GetWarehouseResponseDto();
     public int Year { get; set; }
     public string Number { get; set; } = string.Empty;
-    public GetDocumentsDefinitionResponseDto DocumentDefinition { get; set; } = new GetDocumentsDefinitionResponseDto();
+    public GetDocumentsDefinitionResponseDto DocumentsDefinition { get; set; } = new GetDocumentsDefinitionResponseDto();
     public GetUserResponseDto User { get; set; } = new GetUserResponseDto();
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
