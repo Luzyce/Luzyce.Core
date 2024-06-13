a630d83195fc560acd22773da97baf5e39256dc0
Daniel <daniel.dominik.nowicki@gmail.com>
2024-06-13 13:31:50 +0200

    add GetUserForUpdateDto

---
+++ Luzyce.Core/Models/User/GetUserForUpdateDto.cs
@@ -0,0 +1,12 @@
+namespace Luzyce.Core.Models.User;
+
+public class GetUserForUpdateDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Login { get; set; } = string.Empty;
+    public string Hash { get; set; } = string.Empty;
+    public bool Admin { get; set; }
+}
