9f108e138a6b5c9d570e09f2c0a0c25a5d7b3897
Daniel <daniel.dominik.nowicki@gmail.com>
2024-06-26 10:44:15 +0200

    add production order

---
+++ Luzyce.Core/Models/ProductionOrder/CreateProductionOrderDto.cs
@@ -0,0 +1,9 @@
+﻿using Luzyce.Core.Models.Order;
+
+namespace Luzyce.Core.Models.ProductionOrder;
+
+public class CreateProductionOrderDto
+{
+    public GetOrderResponseDto Order { get; set; } = new GetOrderResponseDto();
+    public List<GetProductionOrderPositionsDto> ProductionOrderPositions { get; set; } = [];
+}
---
+++ Luzyce.Core/Models/ProductionOrder/GetProductionOrderPositionsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Luzyce.Core.Models.ProductionOrder;
+
+public class GetProductionOrderPositionsDto
+{
+    public int DocumentId { get; set; }
+    public string Symbol { get; set; } = string.Empty;
+    public int Gross { get; set; }
+    public int Net { get; set; }
+}
