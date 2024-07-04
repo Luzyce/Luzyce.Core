178c7039d2a741a70b914ac187535ecdfed51779
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-04 16:24:15 +0200

    add new columns

--- Luzyce.Core/Models/ProductionOrder/GetProductionOrderPositions.cs
+++ Luzyce.Core/Models/ProductionOrder/GetProductionOrderPositions.cs
@@ -7,8 +7,13 @@ public class GetProductionOrderPositions
     public int Id { get; set; }
     public int QuantityNetto { get; set; }
     public int QuantityGross { get; set; }
-    public DateTime? CreatedAt { get; set; }
+    public DateTime? ExecutionDate { get; set; }
     public GetLampshade Lampshade { get; set; } = new GetLampshade();
     public GetLampshadeNorm LampshadeNorm { get; set; } = new GetLampshadeNorm();
     public string LampshadeDekor { get; set; } = string.Empty;
+    public string Remarks { get; set; } = string.Empty;
+    public int? NumberOfChanges { get; set; }
+    public int? QuantityMade { get; set; }
+    public string? MethodOfPackaging { get; set; }
+    public int? quantityPerPack { get; set; }
 }
