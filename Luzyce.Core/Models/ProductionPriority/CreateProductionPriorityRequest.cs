efba7b33d666d17f3fbd3ad5af837fd808c312fe
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-10 14:48:31 +0200

    add CreateProductionPriorityRequest

---
+++ Luzyce.Core/Models/ProductionPriority/CreateProductionPriorityRequest.cs
@@ -0,0 +1,8 @@
+﻿using Luzyce.Core.Models.ProductionOrder;
+
+namespace Luzyce.Core.Models.ProductionPriority;
+
+public class CreateProductionPriorityRequest
+{
+    public List<GetProductionOrderPosition> position { get; set; } = [];
+}
