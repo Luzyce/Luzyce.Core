da6ec78d993898c417ef18749e43697a00262b8a
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-29 15:22:40 +0200

    add DateOnly

--- Luzyce.Core/Models/ProductionPlan/GetProductionPlan.cs
+++ Luzyce.Core/Models/ProductionPlan/GetProductionPlan.cs
@@ -6,7 +6,7 @@ namespace Luzyce.Core.Models.ProductionPlan;
 public class GetProductionPlan
 {
     public int Id { get; set; }
-    public DateTime Date { get; set; }
+    public DateOnly Date { get; set; }
     public int Change { get; set; }
     public int Team { get; set; }
     public GetUserResponseDto? Metallurgist { get; set; }
