c2fbcd60faa6030596ce85c0d878fab7f700cbf8
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-30 17:11:09 +0200

    change items to positions

--- Luzyce.Core/Models/ProductionPlan/AddItemsToProductionPlan.cs
+++ Luzyce.Core/Models/ProductionPlan/AddItemsToProductionPlan.cs
@@ -5,5 +5,5 @@ public class AddItemsToProductionPlan
     public DateOnly Date { get; set; }
     public int Change { get; set; }
     public int Team { get; set; }
-    public Dictionary<int, int> Items { get; set; } = new();
+    public Dictionary<int, int> Positions { get; set; } = new();
 }
