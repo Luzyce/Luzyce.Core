8160706435183c599c27a1fb7b872bcde57235c5
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-30 17:02:12 +0200

    add AddItemsToProductionPlan

---
+++ Luzyce.Core/Models/ProductionPlan/AddItemsToProductionPlan.cs
@@ -0,0 +1,9 @@
+﻿namespace Luzyce.Core.Models.ProductionPlan;
+
+public class AddItemsToProductionPlan
+{
+    public DateOnly Date { get; set; }
+    public int Change { get; set; }
+    public int Team { get; set; }
+    public Dictionary<int, int> Items { get; set; } = new();
+}
