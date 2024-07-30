bdfae3124aca15149c333e8f719d553992bbebc0
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-30 19:33:15 +0200

    change document position to production order position

--- Luzyce.Core/Models/ProductionPlan/GetProductionPlanPosition.cs
+++ Luzyce.Core/Models/ProductionPlan/GetProductionPlanPosition.cs
@@ -1,10 +1,10 @@
-﻿using Luzyce.Core.Models.Document;
+﻿using Luzyce.Core.Models.ProductionOrder;
 
 namespace Luzyce.Core.Models.ProductionPlan;
 
 public class GetProductionPlanPosition
 {
     public int Id { get; set; }
-    public GetDocumentPositionResponseDto DocumentPosition { get; set; } = new GetDocumentPositionResponseDto();
+    public GetProductionOrderPosition DocumentPosition { get; set; } = new GetProductionOrderPosition();
     public int? NumberOfHours { get; set; }
 }
