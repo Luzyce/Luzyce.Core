7957a9f294e293b93596a57527dae2d37dafed7b
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-07-31 13:09:41 +0200

    bump version

--- Luzyce.Core/Models/ProductionPlan/GetProductionPlan.cs
+++ Luzyce.Core/Models/ProductionPlan/GetProductionPlan.cs
@@ -9,6 +9,7 @@ public class GetProductionPlan
     public DateOnly Date { get; set; }
     public int Change { get; set; }
     public int Team { get; set; }
-    public GetUserResponseDto? Metallurgist { get; set; }
+    public GetUserResponseDto? ShiftSupervisor { get; set; }
     public GetStatusResponseDto? Status { get; set; }
+    public List<GetProductionPlanPosition> ProductionPlanPositions { get; set; } = [];
 }
