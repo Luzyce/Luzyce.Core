3fa13d89a7fccaeaa298f7752a60600762a72ae4
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-07-31 12:02:58 +0200

    update UpdateInfoInProductionPlanPosition

--- Luzyce.Core/Models/ProductionPlan/UpdateInfoInProductionPlanPosition.cs
+++ Luzyce.Core/Models/ProductionPlan/UpdateInfoInProductionPlanPosition.cs
@@ -3,7 +3,9 @@
 public class UpdateInfoInProductionPlanPosition
 {
     public int PositionId { get; set; }
+    public int? MetallurgistsWorkshopId { get; set; }
     public int? NumberOfHours { get; set; }
     public int? WeightNetto { get; set; }
     public int? WeightBrutto { get; set; }
+    public int? QuantityPerChange { get; set; }
 }
