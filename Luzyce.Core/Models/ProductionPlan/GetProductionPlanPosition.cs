e84ffd911c1a987b0ddc8aa4594d622dc33fddcb
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-08-14 11:12:25 +0200

    add Quantity to GetProductionPlanPosition

--- Luzyce.Core/Models/ProductionPlan/GetProductionPlanPosition.cs
+++ Luzyce.Core/Models/ProductionPlan/GetProductionPlanPosition.cs
@@ -7,6 +7,7 @@ namespace Luzyce.Core.Models.ProductionPlan;
 public class GetProductionPlanPosition
 {
     public int Id { get; set; }
+    public int Quantity { get; set; }
     public GetProductionOrderPosition DocumentPosition { get; set; } = new GetProductionOrderPosition();
     public int? HeadsOfMetallurgicalTeamsId { get; set; }
     public GetUserResponseDto? HeadsOfMetallurgicalTeams { get; set; }
