e3bc5465463837c7ac717bb3c1f2ab0357bbb917
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-08-27 15:00:46 +0200

    add Status to GetOrdersDto.cs

--- Luzyce.Core/Models/Order/GetOrdersDto.cs
+++ Luzyce.Core/Models/Order/GetOrdersDto.cs
@@ -5,4 +5,5 @@ public class GetOrdersDto
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public string? CustomerName { get; set; }
+    public int Status { get; set; }
 }
