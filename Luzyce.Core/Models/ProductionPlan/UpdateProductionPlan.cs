a826c7a0422c78c2c4f428e8afdd1d19c8f49a0c
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-04 14:32:23 +0200

    update UpdateProductionPlan

--- Luzyce.Core/Models/ProductionPlan/UpdateProductionPlan.cs
+++ Luzyce.Core/Models/ProductionPlan/UpdateProductionPlan.cs
@@ -4,6 +4,6 @@ public class UpdateProductionPlan
 {
     public int Id { get; set; }
     public int? ShiftSupervisorId { get; set; }
-    public int? GetHeadsOfMetallurgicalTeamsId { get; set; }
+    public int? HeadsOfMetallurgicalTeamsId { get; set; }
     public List<UpdateInfoInProductionPlanPosition> ProductionPlanPositions { get; set; } = [];
 }
