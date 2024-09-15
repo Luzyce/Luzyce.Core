12db07b51fdb16c0e13bf51a46751341d9c1e943
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-15 14:55:40 +0200

    add CustomerLampshadeNumber

--- Luzyce.Core/Models/ProductionOrder/GetProductionOrderPosition.cs
+++ Luzyce.Core/Models/ProductionOrder/GetProductionOrderPosition.cs
@@ -14,6 +14,7 @@ public class GetProductionOrderPosition
     public GetLampshadeNorm LampshadeNorm { get; set; } = new GetLampshadeNorm();
     public string LampshadeDekor { get; set; } = string.Empty;
     public string Remarks { get; set; } = string.Empty;
+    public string? CustomerLampshadeNumber { get; set; }
     public decimal? NumberOfChanges { get; set; }
     public int? QuantityMade { get; set; }
     public int ProductId { get; set; }
