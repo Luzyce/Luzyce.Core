52f497edfb747ba2bafbb8499f9615893404fdef
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-15 19:29:17 +0200

    add OriginalOrderNumber

--- Luzyce.Core/Models/ProductionOrder/GetProductionOrderForList.cs
+++ Luzyce.Core/Models/ProductionOrder/GetProductionOrderForList.cs
@@ -7,6 +7,7 @@ public class GetProductionOrderForList
     public int Id { get; set; }
     public DateTime OrderDate { get; set; }
     public string OrderNumber { get; set; } = string.Empty;
+    public string? OriginalOrderNumber { get; set; } = string.Empty;
     public string CustomerName { get; set; } = string.Empty;
     public DateTime ProdOrderDate { get; set; }
     public string ProdOrderNumber { get; set; } = string.Empty;
