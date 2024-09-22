2dcce96f7d641a50d7bbe6b751d517b862da4fa0
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-22 17:47:32 +0200

    add nullable to client and log

--- Luzyce.Core/Models/Log/GetClient.cs
+++ Luzyce.Core/Models/Log/GetClient.cs
@@ -3,7 +3,7 @@
 public class GetClient
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; } = string.Empty;
     public string IpAddress { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
 }
--- Luzyce.Core/Models/Log/GetLog.cs
+++ Luzyce.Core/Models/Log/GetLog.cs
@@ -6,8 +6,8 @@ public class GetLog
 {
     public int Id { get; set; }
     public DateTime Timestamp { get; set; }
-    public GetClient Client { get; set; } = new();
+    public GetClient? Client { get; set; } = new();
     public string Operation { get; set; } = string.Empty;
-    public GetUserResponseDto User { get; set; } = new();
-    public string Hash { get; set; } = string.Empty;
+    public GetUserResponseDto? User { get; set; } = new();
+    public string? Hash { get; set; } = string.Empty;
 }
