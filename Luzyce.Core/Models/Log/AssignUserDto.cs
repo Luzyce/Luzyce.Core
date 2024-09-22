015c152b1a2c365d637dadafb279ff39ab07b8db
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-22 20:13:49 +0200

    add AssignUserDto

---
+++ Luzyce.Core/Models/Log/AssignUserDto.cs
@@ -0,0 +1,9 @@
+﻿using Luzyce.Core.Models.User;
+
+namespace Luzyce.Core.Models.Log;
+
+public class AssignUserDto
+{
+    public GetUserResponseDto User { get; set; } = new();
+    public GetLog Log { get; set; } = new();
+}
