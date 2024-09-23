1ef62ad12aeb93d80b548e63e0bf41da782307de
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-23 14:48:24 +0200

    update UpdateKwit

--- Luzyce.Core/Models/Kwit/UpdateKwit.cs
+++ Luzyce.Core/Models/Kwit/UpdateKwit.cs
@@ -3,10 +3,7 @@
 public class UpdateKwit
 {
     public int Id { get; set; }
-    public int StatusId { get; set; }
-    public int? LockedById { get; set; }
     public int QuantityNetto { get; set; } // from KwitPosition
-    public int QuantityGross { get; set; } // from KwitPosition
     public int QuantityLoss { get; set; } // from KwitPosition
     public int QuantityToImprove { get; set; } // from KwitPosition
 }
