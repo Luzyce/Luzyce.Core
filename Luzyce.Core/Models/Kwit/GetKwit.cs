594663e77c0a4f0544c7f79c01e5e0b61b437677
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-23 13:34:25 +0200

    add GetKwit

---
+++ Luzyce.Core/Models/Kwit/GetKwit.cs
@@ -0,0 +1,27 @@
+﻿using Luzyce.Core.Models.Document;
+using Luzyce.Core.Models.Log;
+using Luzyce.Core.Models.ProductionPlan;
+using Luzyce.Core.Models.User;
+
+namespace Luzyce.Core.Models.Kwit;
+
+public class GetKwit
+{
+    public int Id { get; set; }
+    public int DocNumber { get; set; }
+    public GetWarehouseResponseDto? Warehouse { get; set; } = new GetWarehouseResponseDto();
+    public int Year { get; set; }
+    public string? Number { get; set; }
+    public GetDocumentsDefinitionResponseDto? DocumentsDefinition { get; set; } = new GetDocumentsDefinitionResponseDto();
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime? ClosedAt { get; set; }
+    public GetStatusResponseDto? Status { get; set; } = new GetStatusResponseDto();
+    public GetClient LockedBy { get; set; } = new GetClient();
+
+    public int Quantity { get; set; } // from ProductionPlanPosition
+    public int QuantityNetto { get; set; } // from KwitPosition
+    public int QuantityBrutto { get; set; } // from KwitPosition
+    public int QuantityLoss { get; set; } // from KwitPosition
+    public int QuantityToImprove { get; set; } // from KwitPosition
+}
