f9ec0d52fdb5ad24a8dd081222793e2ec6d14aa6
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-09-29 13:48:59 +0200

    add DocumentDependencyChart

---
+++ Luzyce.Core/Models/DocumentDependencyChart/DocumentDependencyChart.cs
@@ -0,0 +1,6 @@
+﻿namespace Luzyce.Core.Models.DocumentDependencyChart;
+
+public class DocumentDependencyChart
+{
+    public DocumentForChart? Document { get; set; }
+}
---
+++ Luzyce.Core/Models/DocumentDependencyChart/DocumentForChart.cs
@@ -0,0 +1,10 @@
+﻿namespace Luzyce.Core.Models.DocumentDependencyChart;
+
+public class DocumentForChart
+{
+    public int Id { get; set; }
+
+    public string DocumentType { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public List<DocumentForChart>? Derivatives { get; set; } = null;
+}
