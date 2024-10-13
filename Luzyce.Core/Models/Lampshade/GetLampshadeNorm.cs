3389ac862f3ee04aad0d43c0d0c57ad26f6a144c
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-10-13 17:29:48 +0200

    change double to decimal in Weight

--- Luzyce.Core/Models/Lampshade/GetLampshadeNorm.cs
+++ Luzyce.Core/Models/Lampshade/GetLampshadeNorm.cs
@@ -6,8 +6,8 @@ public class GetLampshadeNorm
     public GetLampshade Lampshade { get; set; } = new GetLampshade();
     public GetVariantResponseDto Variant { get; set; } = new GetVariantResponseDto();
     public int QuantityPerChange { get; set; }
-    public double? WeightBrutto { get; set; }
-    public double? WeightNetto { get; set; }
+    public decimal? WeightBrutto { get; set; }
+    public decimal? WeightNetto { get; set; }
     public string? MethodOfPackaging { get; set; } = string.Empty;
     public int? QuantityPerPack { get; set; }
 }
--- Luzyce.Core/Models/ProductionPlan/UpdateInfoInProductionPlanPosition.cs
+++ Luzyce.Core/Models/ProductionPlan/UpdateInfoInProductionPlanPosition.cs
@@ -5,7 +5,7 @@ public class UpdateInfoInProductionPlanPosition
     public int Id { get; set; }
     public int Quantity { get; set; }
     public int? NumberOfHours { get; set; }
-    public double? WeightNetto { get; set; }
-    public double? WeightBrutto { get; set; }
+    public decimal? WeightNetto { get; set; }
+    public decimal? WeightBrutto { get; set; }
     public int? QuantityPerChange { get; set; }
 }
