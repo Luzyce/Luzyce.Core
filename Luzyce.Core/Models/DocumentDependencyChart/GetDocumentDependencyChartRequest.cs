f1c12f452d130f57b0186b44e83d8f2e73183919
Daniel Nowicki <daniel.dominik.nowicki@gmail.com>
2024-10-13 11:53:55 +0200

    add GetDocumentDependencyChartRequest

---
+++ Luzyce.Core/Models/DocumentDependencyChart/GetDocumentDependencyChartRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Luzyce.Core.Models.DocumentDependencyChart;
+
+public class GetDocumentDependencyChartRequest
+{
+    public int DocumentId { get; set; }
+    public string DocumentType { get; set; } = string.Empty;
+}
